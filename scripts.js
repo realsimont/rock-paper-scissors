d401a94ffd941500a180b602bc2229333261580a
Simon Thompson <simonct.sct@gmail.com>
2021-01-16 13:57:19 +0800

    Refactored for better readability

--- scripts.js
+++ scripts.js
@@ -1,73 +1,59 @@
-let games = 1; //start at 1 and go to 11 for 10 games
+// initialise starting scores and make available through game
+let games = 1; //start at 1 to count normally when we print this
 let humanScore = 0;
 let computerScore = 0;
 let ties = 0;
 
-function refresh() { //simple clear all function
-    location.reload();
+function playRound(humanSelection) {
+
+  let computerSelection;
+  function computerSelect() {
+    random = Math.floor(Math.random() * 3);
+    if (random === 0) { computerSelection = "Rock";}
+    else if (random === 1) { computerSelection = "Paper"; }
+    else if (random === 2) { computerSelection = "Scissors"; }
+  }
+
+  computerSelect();
+  if (humanSelection === computerSelection) {
+    printResultOnPage(`Game ${games}: You played ${humanSelection}, Computer played ${computerSelection} - it's a tie!`);
+    games++;
+    ties++;
+  } else if ( // human wins
+    (humanSelection === "Rock" && computerSelection === "Scissors") ||
+    (humanSelection === "Paper" && computerSelection === "Rock") ||
+    (humanSelection === "Scissors" && computerSelection === "Paper")
+    ) {
+    printResultOnPage(`Game ${games}: You played ${humanSelection}, Computer played ${computerSelection} - you lose!`);
+    games++;
+    humanScore++;
+  } else { // computer wins
+    printResultOnPage(`Game ${games}: You played ${humanSelection}, Computer played ${computerSelection} - you win!`);
+    games++;
+    computerScore++;
+  }
+  if (games === 11) { gameOver(); }
+
+  function gameOver() {
+    printResultOnPage(`That's it! Game over. After 10 games, the final score is: You ${humanScore}, Computer ${computerScore}, Ties: ${ties}.`);
+    // document.querySelectorAll(".btn").classList.add("disabled-btn");
+    btns = document.querySelectorAll(".btn")
+    for (i = 0; i < btns.length; i++) {
+      btns[i].classList.add("disabled-btn");
+    }
+    startOver = document.createElement("p");
+    startOver.innerHTML = 'Click "Start Over" to play again.';
+    resultsArea.appendChild(startOver);
+  }
+
+  function printResultOnPage(outcome) {
+    resultsArea = document.getElementById("results");
+    result = document.createElement("p");
+    result.innerHTML = outcome;
+    resultsArea.appendChild(result);
+  }
 }
 
-function playRound(humanSelection) {
-    let computerSelection;
-    function computerSelect() {
-        random = Math.floor(Math.random() * 3);
-        if (random === 0) {
-            computerSelection = "Rock";
-        } else if (random === 1) {
-            computerSelection = "Paper";
-        } else if (random === 2) {
-            computerSelection = "Scissors";
-        }
-    }
-    computerSelect();
-    console.log(games);
-    let humanWinOutcome = `Game ${games}: You played ${humanSelection}, Computer played ${computerSelection} - you win!`;
-    let humanLoseOutcome = `Game ${games}: You played ${humanSelection}, Computer played ${computerSelection} - you lose!`;
-    let tieOutcome = `Game ${games}: You played ${humanSelection}, Computer played ${computerSelection} - it's a tie!`;
-    let finalOutcome = `That's it! Game over. After 10 games, the final score is: You ${humanScore}, Computer ${computerScore}, Ties: ${ties}.`;
-    function printResult(outcome) {
-        resultsArea = document.getElementById("results");
-        result = document.createElement("p");
-        result.innerHTML = outcome;
-        resultsArea.appendChild(result);
-    }
-    function gameOver() {
-        printResult(finalOutcome);
-        document.getElementById("rock").classList.add("disabled-btn");
-        document.getElementById("paper").classList.add("disabled-btn");
-        document.getElementById("scissors").classList.add("disabled-btn");
-        startOver = document.createElement("p");
-        startOver.innerHTML = 'Click "Start Over" to play again.';
-        resultsArea.appendChild(startOver);
-    }
-    if ( //human wins
-        (humanSelection === "Rock" && computerSelection === "Scissors") ||
-        (humanSelection === "Paper" && computerSelection === "Rock") ||
-        (humanSelection === "Scissors" && computerSelection === "Paper")
-        ) {
-        printResult(humanWinOutcome);
-        games++;
-        humanScore++;
-        if (games === 11) {
-            gameOver();
-        }
-    } else if ( //computer wins
-        (computerSelection === "Rock" && humanSelection === "Scissors") ||
-        (computerSelection === "Paper" && humanSelection === "Rock") ||
-        (computerSelection === "Scissors" && humanSelection === "Paper") 
-    ) {
-        printResult(humanLoseOutcome);
-        games++;
-        computerScore++;
-        if (games === 11) {
-            gameOver();
-        }
-    } else { //tie
-        printResult(tieOutcome);
-        games++;
-        ties++;
-        if (games === 11) {
-            gameOver();
-        }
-    }
+function refresh() { //simple clear all function
+  location.reload();
 }
